1251e8e883cea51d7072479d41cb198e4b30afd0
saurabh ranjan <saurabhranjan1806@gmail.com>
2018-05-27 21:43:21 +0530

    trying ipc for multiple windows and communication btw them

--- src/main.js
+++ src/main.js
@@ -5,25 +5,33 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain
 
-let mainWindow
+// let mainWindow
+const windows = []
+
 
 app.on('ready', _ => {
-    console.log('ready!')
-    mainWindow = new BrowserWindow({
-        height: 400,
-        width: 400
-    })
+    // console.log('ready!')
+    [1, 2, 3].forEach((_) => {
+        console.log('ready!')
+        let win = new BrowserWindow({
+            height: 400,
+            width: 400
+        })
 
-    mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+        win.loadURL(`file://${__dirname}/countdown.html`)
 
-    mainWindow.on('closed', _ => {
-        console.log('closed!')
-        mainWindow = null
+        win.on('closed', _ => {
+            console.log('closed!')
+            win = null
+        })
+        windows.push(win)
     })
 })
 
 ipc.on('countdown-start', () => {
     countdown((count) => {
-        mainWindow.webContents.send('countdown', count)
+        windows.forEach((win) => {
+            win.webContents.send('countdown', count)
+        })
     })
 })
